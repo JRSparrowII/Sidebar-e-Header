69f72d43ef4431969513984fc6d29df0afb9600b
Carlos Henrique <you@example.comcarloshenriquepvh@hotmail.com>
2022-09-15 13:27:08 -0400

    testando bug no  git

--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -1,6 +1,7 @@
-import { Text, Link, Icon, LinkProps as ChakraLinkProps } from '@chakra-ui/react'
+import { Text, Link as ChakraLink, Icon, LinkProps as ChakraLinkProps } from '@chakra-ui/react'
 import { ElementType } from 'react';
 import { RiDashboardLine } from 'react-icons/ri'
+import Link from 'next/link'
 
 interface NavLinkProps extends ChakraLinkProps{
     icon: ElementType,
@@ -9,9 +10,12 @@ interface NavLinkProps extends ChakraLinkProps{
 
 export function NavLink({icon, children, ...rest}: NavLinkProps){
     return(
-        <Link display="flex" alignItems="center" {...rest}>
-            <Icon as={icon} fontSize="20"/> 
-            <Text ml="4" fontWeight="medium">{children}</Text>                     
-        </Link>    
+        <Link >
+            <ChakraLink display="flex" alignItems="center" {...rest}>
+                <Icon as={icon} fontSize="20"/> 
+                <Text ml="4" fontWeight="medium">{children}</Text>                     
+            </ChakraLink> 
+        </Link>
+           
     );
 }
